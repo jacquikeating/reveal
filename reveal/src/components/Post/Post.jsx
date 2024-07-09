771dd0da9afe913b2eba6fa745229959687040f4
jacquikeating <j.keating@live.ca>
2024-07-09 18:43:56 -0400

    Added icons and timestamp to Post component

--- reveal/src/components/Post/Post.jsx
+++ reveal/src/components/Post/Post.jsx
@@ -9,10 +9,24 @@ const Post = () => {
         className="post__avatar"
       />
       <p className="post__username">Display Name</p>
+      <p className="post__timestamp">Jan 1 2025</p>
       <div className="post__spacer"></div>
       <p className="post__content">
         I'm the hottest post on this site! (I'm also the only post.)
       </p>
+      <div className="post__spacer"></div>
+      <div className="post__reactions">
+        <img
+          className="post__icon post__icon--likes"
+          src="../src/assets/icons/heart.svg"
+        />
+        <p className="post__likes">0</p>
+        <img
+          className="post__icon post__icon--comments"
+          src="../src/assets/icons/comment.svg"
+        />
+        <p className="post__comments">0</p>
+      </div>
     </article>
   );
 };
