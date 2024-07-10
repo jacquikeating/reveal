e2d4b21b0746089b53dd0b0ddc1c5948f6c95dcf
jacquikeating <j.keating@live.ca>
2024-07-09 21:30:08 -0400

    Added footer links
    - Added <Link> elements to Footer nav icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <img src="../src/assets/icons/nav-profile.svg" className="footer__icon" />
-      <img src="../src/assets/icons/nav-home.svg" className="footer__icon" />
-      <img src="../src/assets/icons/nav-events.svg" className="footer__icon" />
+      <Link to="profile">
+        <img
+          src="../src/assets/icons/nav-profile.svg"
+          className="footer__icon"
+        />
+      </Link>
+      <Link to="/">
+        <img src="../src/assets/icons/nav-home.svg" className="footer__icon" />
+      </Link>
+      <Link to="events">
+        <img
+          src="../src/assets/icons/nav-events.svg"
+          className="footer__icon"
+        />
+      </Link>
     </footer>
   );
 };
