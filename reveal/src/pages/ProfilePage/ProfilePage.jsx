be0483624fd5d85577ee1cc4dd5ad8627acfff6f
jacquikeating <j.keating@live.ca>
2024-07-09 20:17:42 -0400

    Standardized section borders
    - Migrated styles for the section divider from HomePage.scss to App.scss in order to apply the same divider globally
    - Adjusted padding on sections
    - temporary styling on h1 and h2

--- reveal/src/pages/ProfilePage/ProfilePage.jsx
+++ reveal/src/pages/ProfilePage/ProfilePage.jsx
@@ -9,14 +9,25 @@ const ProfilePage = () => {
     <>
       <Hero />
       <main className="profile">
-        <h1 className="profile__name">Display Name</h1>
-        <p className="profile__bio">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis,
-          explicabo iure saepe aliquid libero dolorum maxime nam eligendi
-          cumque. Excepturi quia error qui beatae iusto odit expedita molestias
-          at fuga?
-        </p>
-        <Socials />
+        <section className="profile__section">
+          <h1 className="profile__name">Display Name</h1>
+          <p className="profile__bio">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis,
+            explicabo iure saepe aliquid libero dolorum maxime nam eligendi
+            cumque. Excepturi quia error qui beatae iusto odit expedita
+            molestias at fuga?
+          </p>
+          <Socials />
+        </section>
+
+        <section className="profile__section">
+          <h2 className="profile__section-heading">Events</h2>
+          <EventPreview />
+        </section>
+
+        <section className="profile__section">
+          <h2 className="profile__section-heading">Gallery</h2>
+        </section>
       </main>
     </>
   );
