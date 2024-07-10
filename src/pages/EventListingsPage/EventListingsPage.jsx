aaca70af3c4202e65812cdd54519f892b34c8282
jacquikeating <j.keating@live.ca>
2024-07-10 10:38:35 -0400

    Added month pagination to EventListingsPage
    - Added a "month" subheading to EventListingsPage, with two arrows for pagination
    - Pagination not currently functional

--- src/pages/EventListingsPage/EventListingsPage.jsx
+++ src/pages/EventListingsPage/EventListingsPage.jsx
@@ -31,9 +31,22 @@ const EventListingsPage = () => {
   ];
 
   return (
-    <main>
-      <section>
-        <h1>Upcoming Events in City</h1>
+    <main className="events-list">
+      <section className="events-list__section">
+        <h1 className="events-list__heading">Upcoming Events in City</h1>
+        <div className="events-list__pagination">
+          <img
+            src="/src/assets/icons/arrow-left.svg"
+            alt="Arrow pointing left"
+            className="events-list__arrow"
+          />
+          <h2 className="events-list__month-subheading">Month</h2>
+          <img
+            src="/src/assets/icons/arrow-right.svg"
+            alt="Arrow pointing right"
+            className="events-list__arrow"
+          />
+        </div>
         {eventsData.map((show) => {
           return (
             <EventPreview
