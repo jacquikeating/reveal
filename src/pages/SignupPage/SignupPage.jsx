0c901e90bd24452b93651ebdf59ffe9d7f7400eb
jacquikeating <j.keating@live.ca>
2024-07-11 01:39:03 -0400

    Added <section> and classNames to SignupPage
    - Setting up for styling to be applied tomorrow

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,11 +1,14 @@
 import "./SignupPage.scss";
 import SignupForm from "../../components/SignupForm/SignupForm";
+import FileUploader from "../../components/FileUploader/FileUploader";
 
 const SignupPage = () => {
   return (
-    <main>
-      <h1>Sign Up for Reveal</h1>
-      <SignupForm />
+    <main className="signup-page">
+      <section className="signup-page__section">
+        <h1 className="signup-page__heading">Sign Up for Reveal</h1>
+        <SignupForm />
+      </section>
     </main>
   );
 };
