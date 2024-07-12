610708848fe8d3b70bdff09894d0110bacb1eb3c
jacquikeating <j.keating@live.ca>
2024-07-12 11:00:02 -0400

    Universal transition and small bug fix
    - Removed transition: all linear 0.2s from PerformersList to apply globally instead
    - Bug fix: Logo was "wiggling" when performer avatars were hovered over, due to their transform: scale. Adding a wrapper around the logo prevents this.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Logo.scss";
 
 const Logo = () => {
-  return <p className="logo">Reveal</p>;
+  return (
+    <div className="logo__wrapper">
+      <p className="logo">Reveal</p>
+    </div>
+  );
 };
 
 export default Logo;
