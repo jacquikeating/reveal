512499e1cc972c6a443490dbf2806932918b655c
jacquikeating <j.keating@live.ca>
2024-07-13 14:13:44 -0400

    EventDetailsPage renders more information
    - Added basic HTML structure for additional info on EventDetailsPages
    - Includes tables for times and ticket prices
    - Text elements for location and producer
    - Button that links to external page to purchase tickets
    - Commented out gallery section since it is not included in MVP

--- src/components/PerformersList/PerformersList.jsx
+++ src/components/PerformersList/PerformersList.jsx
@@ -9,20 +9,32 @@ const PerformersList = ({ performerIDs, allUsersList }) => {
 
   return (
     <ul className="performers-list">
-      {performersInShow.map((performer) => {
-        return (
-          <li className="performer" key={performer.id}>
-            <Link to={`/profile/${performer.id}`} className="performer__link">
-              <img
-                className="performer__avatar"
-                src={performer.avatar}
-                alt="Performer's avatar"
-              />
-              <p className="performer__name">{performer.name}</p>
-            </Link>
-          </li>
-        );
-      })}
+      {
+        // If the organizer has not yet provided their list of performers, display a fallback message
+        performersInShow.length === 0 ? (
+          <p className="perfomers-list__fallback">
+            Lineup to be announced. Check back soon!
+          </p>
+        ) : (
+          performersInShow.map((performer) => {
+            return (
+              <li className="performer" key={performer.id}>
+                <Link
+                  to={`/profile/${performer.id}`}
+                  className="performer__link"
+                >
+                  <img
+                    className="performer__avatar"
+                    src={performer.avatar}
+                    alt="Performer's avatar"
+                  />
+                  <p className="performer__name">{performer.name}</p>
+                </Link>
+              </li>
+            );
+          })
+        )
+      }
     </ul>
   );
 };
--- src/pages/EventDetailsPage/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage/EventDetailsPage.jsx
@@ -32,15 +32,17 @@ const EventDetailsPage = () => {
     doors_time,
     start_time,
     end_time,
-    producer,
+    organizer, // remember to rename in sql/knex too
     ticket_prices,
     performers,
     buy_tickets,
     gallery,
   } = eventData;
+  const { GA, advanceGA, VIP, advanceVIP, tableDiscounts } =
+    eventData.ticket_prices;
 
   useEffect(() => {
-    const fetchEventData = async (req) => {
+    const fetchEventData = async (req, res) => {
       try {
         const response = await axios.get(getSingleEventEndpoint(eventID));
         setEventData(response.data);
@@ -83,8 +85,84 @@ const EventDetailsPage = () => {
             topOffset="-3rem"
             leftOffset="0.9rem"
           />
-          <h1 className="event__name">{name}</h1>
-          <p className="event__description">{description}</p>
+          <div className="event__what">
+            <h1 className="event__name">{name}</h1>
+            <p className="event__description">{description}</p>
+          </div>
+
+          <div className="event__where">
+            <h3 className="event__info-category-subheading">Location</h3>
+            <p className="event__city">{city}</p>
+            <p className="event__venue">{venue}</p>
+            <p className="event__venue-address">{venue_address}</p>
+          </div>
+
+          {/* These tables use ternary operators to render rows only for truthy values. 
+              If any value is not specified (e.g. no end time provided, or no early bird discounts),
+              we won't bother rendering a table row for it. */}
+
+          {/* prettier-ignore */ }
+          <div className="event__when">
+            <h3 className="event__info-category-subheading">Time</h3>
+            <table className="timetable">
+              <tbody>
+                {doors_time ? (
+                  <tr className="timetable__row">
+                    <td className="timetable__label">Doors Open</td>
+                    <td className="timetable__time">{doors_time}</td>
+                  </tr>) : ("")}
+                {start_time ? (
+                  <tr className="timetable__row">
+                    <td className="timetable__label">Start Time</td>
+                    <td className="timetable__time">{start_time}</td>
+                  </tr>) : ("")}
+                {end_time ? (
+                  <tr className="timetable__row">
+                    <td className="timetable__label">End Time</td>
+                    <td className="timetable__time">{end_time}</td>
+                  </tr>) : ("")}
+              </tbody>
+            </table>
+          </div>
+
+          {/* prettier-ignore */ }
+          <div className="event__how-much">
+            <h3 className="event__info-category-subheading">Ticket Prices</h3>
+            <table className="tickets-table">
+              <tbody>
+                {advanceGA ? (
+                  <tr className="tickets-table__row">
+                    <td className="tickets-table__label">Advance General Admission</td>
+                    <td className="tickets-table__time">${advanceGA}</td>
+                  </tr>) : ("")}
+                {GA ? (
+                  <tr className="tickets-table__row">
+                    <td className="tickets-table__label">General Admission</td>
+                    <td className="tickets-table__time">${GA}</td>
+                  </tr>) : ("")}
+                  {advanceVIP ? (
+                  <tr className="tickets-table__row">
+                    <td className="tickets-table__label">Advance VIP</td>
+                    <td className="tickets-table__time">${advanceVIP}</td>
+                  </tr>) : ("")}
+                  {VIP ? (
+                  <tr className="tickets-table__row">
+                    <td className="tickets-table__label">VIP</td>
+                    <td className="tickets-table__time">${VIP}</td>
+                  </tr>) : ("")}
+              </tbody>
+            </table>
+            {tableDiscounts ? (
+              <p>Table discounts available</p>
+            ) : ("")}
+          </div>
+          <a href={buy_tickets} target="_blank">
+            <button className="buy__tickets">Buy Tickets</button>
+          </a>
+
+          <div className="event__who">
+            <p>Produced by {organizer}</p>
+          </div>
         </section>
 
         <section className="event__section">
@@ -95,12 +173,12 @@ const EventDetailsPage = () => {
           />
         </section>
 
-        <section className="event__section">
+        {/* <section className="event__section">
           <h2 className="event__section-heading">Gallery</h2>
-          {/* <Suspense fallback={<p>Loading images...</p>}>
+          <Suspense fallback={<p>Loading images...</p>}>
             <Gallery gallery={eventData.gallery} />
-          </Suspense> */}
-        </section>
+          </Suspense>
+        </section> */}
 
         <section className="event__section">
           <h2 className="event__section-heading">Posts</h2>
