57522902723ebc8083dbad7a15cdb21af92e6d76
jacquikeating <j.keating@live.ca>
2024-07-14 14:54:38 -0400

    Nav icon improvements
    - Post icon added to footer
    - Links to /post
    - Icons scaled down slightly
    - Text labels added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,18 +10,28 @@ const Footer = () => {
           src="../src/assets/icons/simple-profile.svg"
           className="footer__icon footer__icon--profile"
         />
+        <p className="footer__label">Profile</p>
       </Link>
       <Link to="/">
         <img
           src="../src/assets/icons/simple-home.svg"
           className="footer__icon"
         />
+        <p className="footer__label">Home</p>
       </Link>
       <Link to="events">
         <img
           src="../src/assets/icons/simple-events.svg"
           className="footer__icon"
         />
+        <p className="footer__label">Events</p>
+      </Link>
+      <Link to="post">
+        <img
+          src="../src/assets/icons/simple-post.svg"
+          className="footer__icon"
+        />
+        <p className="footer__label">Post</p>
       </Link>
     </footer>
   );
