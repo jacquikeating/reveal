2c90d856a133500c3556dbf539e050996c160ac8
jacquikeating <j.keating@live.ca>
2024-07-13 20:16:21 -0400

    Small changes
    - Post cleaned up in structure and style
    - Max-width and margin: auto set on large blocks of text

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -7,24 +7,24 @@ const Post = ({ postData }) => {
   return (
     <article className="post">
       <img className="post__avatar" alt={`${username}'s avatar`} src={avatar} />
-      <p className="post__username">{username}</p>
-      <p className="post__timestamp">{timestamp}</p>
-
-      <div className="post__spacer"></div>
-      <p className="post__content">{content}</p>
-
-      <div className="post__spacer"></div>
-      <div className="post__reactions">
-        <img
-          className="post__icon post__icon--likes"
-          src="../src/assets/icons/heart.svg"
-        />
-        <p className="post__likes">{likes}</p>
-        <img
-          className="post__icon post__icon--comments"
-          src="../src/assets/icons/comment.svg"
-        />
-        <p className="post__comments">{comments.length}</p>
+      <div className="post__text">
+        <div className="post__name-and-time">
+          <p className="post__username">{username}</p>
+          <p className="post__timestamp">{timestamp}</p>
+        </div>
+        <p className="post__content">{content}</p>
+        <div className="post__reactions">
+          <img
+            className="post__icon post__icon--likes"
+            src="../src/assets/icons/heart.svg"
+          />
+          <p className="post__likes">{likes}</p>
+          <img
+            className="post__icon post__icon--comments"
+            src="../src/assets/icons/comment.svg"
+          />
+          <p className="post__comments">{comments.length}</p>
+        </div>
       </div>
     </article>
   );
