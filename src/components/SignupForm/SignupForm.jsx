d609bdad9b2cf24d7f048620fcf9dab46186a7b8
jacquikeating <j.keating@live.ca>
2024-07-14 21:18:48 -0400

    Signup form
    - Uses similar style and functionality to login form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./SignupForm.scss";
 
 const SignupForm = () => {
+  let navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate("/");
+  }
+
+  function goHome() {
+    navigate("/");
+  }
+
   return (
     <form className="signup-form">
       <input
@@ -9,31 +22,41 @@ const SignupForm = () => {
         placeholder="Email"
         name="email"
         className="signup-form__field"
+        aria-label="Enter your email"
       />
       <input
         type="text"
         placeholder="Display Name"
         name="name"
         className="signup-form__field"
+        aria-label="Enter a display name"
       />
       <input
         type="password"
         placeholder="Password"
         name="password"
         className="signup-form__field"
+        aria-label="Enter your password"
       />
       <input
         type="password"
         placeholder="Confirm Password"
         name="confirm-password"
         className="signup-form__field"
+        aria-label="Confirm password"
       />
 
-      <button type="submit" className="signup-form__submit-btn">
+      <button
+        type="submit"
+        className="signup-form__submit-btn"
+        onClick={handleSubmit}
+      >
         Sign Up
       </button>
 
-      <button className="signup-form__cancel-btn">Cancel</button>
+      <button className="signup-form__cancel-btn" onClick={goHome}>
+        Cancel
+      </button>
     </form>
   );
 };
