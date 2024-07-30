028b3b2e26e6c64c0cbef8c03f6b12f0472b8fae
jacquikeating <j.keating@live.ca>
2024-07-29 23:36:29 -0400

    Functionality added to save button
    - Save button first checks if there ARE changes (to cut down on unnecessary writes), then updates the user document with the new values
    - Database gets updated, but I need to figure out how to make it fetch the data again to display it

--- src/pages/EditProfilePage/EditProfilePage.jsx
+++ src/pages/EditProfilePage/EditProfilePage.jsx
@@ -1,6 +1,8 @@
 import "./EditProfilePage.scss";
 import React, { useState, useEffect, Suspense, lazy } from "react";
 import { Link, useParams } from "react-router-dom";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase.js";
 import Hero from "../../components/Hero/Hero";
 import EditSocials from "../../components/EditSocials/EditSocials";
 import EmblaCarousel from "../../components/EmblaCarousel/EmblaCarousel";
@@ -8,24 +10,56 @@ import PostsContainer from "../../components/PostsContainer/PostsContainer";
 const Gallery = lazy(() => import("../../components/Gallery/Gallery"));
 
 const EditProfilePage = ({ userData }) => {
-  function saveData() {
-    console.log("Changes saved!");
+  const [name, setName] = useState(userData.name);
+  const [bio, setBio] = useState(userData.bio);
+
+  const userRef = doc(db, "users", userData.uid);
+  let valuesToUpdate = [];
+  let bbb = {};
+
+  function aaa() {
+    for (let i = 0; i < valuesToUpdate.length; i++) {
+      bbb[valuesToUpdate[i]] = valuesToUpdate[i];
+    }
+
+    console.log(bbb);
   }
 
+  async function saveData() {
+    if (name !== userData.name) {
+      valuesToUpdate.push("name");
+    }
+    if (bio !== userData.bio) {
+      valuesToUpdate.push("bio");
+    }
+
+    // console.log(valuesToUpdate);
+    aaa();
+
+    if (valuesToUpdate) {
+      await updateDoc(userRef, {
+        bbb,
+      });
+    }
+  }
   return (
     <>
       {userData ? (
         <>
-          {" "}
           <Hero img={userData.cover_photo} />
           <main className="edit-profile">
             <section className="edit-profile__section">
               <input
                 type="text"
                 className="edit-profile__name"
-                value={userData.name}
+                defaultValue={name}
+                onChange={(e) => setName(e.target.value)}
               />
-              <textarea className="edit-profile__bio">{userData.bio}</textarea>
+              <textarea
+                className="edit-profile__bio"
+                defaultValue={bio}
+                onChange={(e) => setBio(e.target.value)}
+              ></textarea>
 
               <EditSocials />
             </section>
