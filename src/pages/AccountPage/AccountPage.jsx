8eee5f31035865c37906b636b0cd0903e70e6694
jacquikeating <j.keating@live.ca>
2024-07-30 20:44:24 -0400

    Fixed AccountPage alignment issues
     - Buttons were not aligning due to the fact that two are nested inside <Link> and one is not
    - Standardized by nesting each inside a div
    - Fixed distribution

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -26,31 +26,39 @@ const AccountPage = ({ userData }) => {
           <>
             <p className="acct-page__welcome">Welcome back, {userData.name}!</p>
             <div className="acct-page__btns-container">
-              <button className="acct-page__btn" onClick={logOut}>
-                <img
-                  src="/src/assets/icons/key.svg"
-                  className="acct-page__icon"
-                />
-                Log Out
-              </button>
-              <Link to={`/profile/${userData.profileURL}`}>
-                <button className="acct-page__btn">
+              <div className="acct-page__btn-wrapper">
+                <button className="acct-page__btn" onClick={logOut}>
                   <img
-                    src="/src/assets/icons/mirror.svg"
+                    src="/src/assets/icons/key.svg"
                     className="acct-page__icon"
                   />
-                  View Profile
+                  Log Out
                 </button>
-              </Link>
-              <Link to="/profile/edit">
-                <button className="acct-page__btn">
-                  <img
-                    src="/src/assets/icons/edit.svg"
-                    className="acct-page__icon acct-page__icon--edit"
-                  />
-                  Edit Profile
-                </button>
-              </Link>
+              </div>
+
+              <div className="acct-page__btn-wrapper">
+                <Link to={`/profile/${userData.profileURL}`}>
+                  <button className="acct-page__btn">
+                    <img
+                      src="/src/assets/icons/mirror.svg"
+                      className="acct-page__icon"
+                    />
+                    View Profile
+                  </button>
+                </Link>
+              </div>
+
+              <div className="acct-page__btn-wrapper">
+                <Link to="/profile/edit">
+                  <button className="acct-page__btn">
+                    <img
+                      src="/src/assets/icons/edit.svg"
+                      className="acct-page__icon acct-page__icon--edit"
+                    />
+                    Edit Profile
+                  </button>
+                </Link>
+              </div>
             </div>
           </>
         ) : (
