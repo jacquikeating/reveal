b92a443c6e46a9e6dfd6a3c6ec4ac5496c9e7958
jacquikeating <j.keating@live.ca>
2024-07-31 20:42:02 -0400

    Design changes (Footer & icon hovers)
    - Commented out a lot of the transform: scale on hover things because it was starting to look like Too Much
    - Adjusted footer sizing
    - Footer icons move to header on desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,34 +6,39 @@ const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <Link to="account">
-          <img
-            src="../../src/assets/icons/simple-profile.svg"
-            className="footer__icon footer__icon--profile"
-          />
-          <p className="footer__label">Account</p>
-        </Link>
-        <Link to="/">
-          <img
-            src="../../src/assets/icons/simple-home.svg"
-            className="footer__icon"
-          />
-          <p className="footer__label">Home</p>
-        </Link>
-        <Link to="events">
-          <img
-            src="../../src/assets/icons/simple-events.svg"
-            className="footer__icon"
-          />
-          <p className="footer__label">Events</p>
-        </Link>
-        <Link to="post">
-          <img
-            src="../../src/assets/icons/simple-post.svg"
-            className="footer__icon"
-          />
-          <p className="footer__label">Post</p>
-        </Link>
+        <div className="footer__section">
+          <Link to="account">
+            <img
+              src="../../src/assets/icons/simple-profile.svg"
+              className="footer__icon footer__icon--profile"
+            />
+            <p className="footer__label">Account</p>
+          </Link>
+          <Link to="/">
+            <img
+              src="../../src/assets/icons/simple-home.svg"
+              className="footer__icon"
+            />
+            <p className="footer__label">Home</p>
+          </Link>
+        </div>
+
+        <div className="footer__section">
+          <Link to="events">
+            <img
+              src="../../src/assets/icons/simple-events.svg"
+              className="footer__icon"
+            />
+            <p className="footer__label">Events</p>
+          </Link>
+          <Link to="post">
+            <img
+              src="../../src/assets/icons/simple-post.svg"
+              className="footer__icon"
+            />
+            <p className="footer__label">Post</p>
+          </Link>
+        </div>
       </footer>
     </>
   );
