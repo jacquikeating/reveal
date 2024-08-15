50a83ac1ca79467b34db7654fb8347accbf30298
jacquikeating <j.keating@live.ca>
2024-08-14 20:49:22 -0400

    Added file uploader modal to Gallery
    - Added functionality for the "Add Content" button seen on EditProfilePage that opens a modal with a file picker input
    - Allows user to upload new photos

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import VideoPlayer from "/src/components/VideoPlayer/VideoPlayer.jsx";
 import Image from "/src/components/Image/Image.jsx";
+import FileUploader from "../FirestoreUpload/FirestoreUpload";
+import { Modal } from "react-responsive-modal";
+
 import "./Gallery.scss";
 
 const Gallery = ({ gallery, showEdit }) => {
+  const [open, setOpen] = useState(false);
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  function openModal(event) {
+    onOpenModal();
+  }
+
   return (
     <div className="gallery">
       {gallery
@@ -19,7 +31,13 @@ const Gallery = ({ gallery, showEdit }) => {
 
       {showEdit ? (
         <>
-          <button className="gallery__add-btn">Add Content</button>
+          <button className="gallery__add-btn" onClick={openModal}>
+            Add Content
+          </button>
+          <Modal open={open} onClose={onCloseModal} center>
+            <FileUploader></FileUploader>
+          </Modal>
+
           {/* <button className="gallery__del-btn">Delete</button> */}
         </>
       ) : (
