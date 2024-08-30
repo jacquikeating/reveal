401a00e908767d24d1f8e9075c8c3c81ce470453
jacquikeating <j.keating@live.ca>
2024-08-29 22:29:54 -0400

    Change cover image functionality implemented
    - Added necessary JS functions to allow the user to change their cover image, using the modal built & styled in previous commit.
    - Added ability for Hero component to read and write userData in localStorage and Firestore.
    - Added coverImg and selectedImg states.
    - The coverImg state initializes as the value of coverImg from Firestore.
    - selectedImg simply applies a CSS class (white border + opacity increase) to highlight the currently selected image. This initializes as the existing coverImg, and is updated whenever the user clicks a different image in the modal.
    - Added a "save changes" button that calls the function changeCover()
    - changeCover() first checks whether selectedImg matches coverImg. If they do, it means the user has not selected a different image, so nothing needs to happen aside from closing the modal. If they are different, the function takes selectedImg and sets it as the value of coverImg. It then updates userData in localStorage and Firestore, and closes the modal.
    - imgSrc variable now first checks if a coverImg from localStorage is available; if not, it uses the img prop passed down from the parent; if that is null, it defaults to the placeholder image. Because its preferred option is a state variable, the component will instantly re-render when coverImg changes.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,49 @@
 import { React, useState } from "react";
 import { Modal } from "react-responsive-modal";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase.js";
 import "./Hero.scss";
 
 const Hero = ({ img, nextEvent, showEdit }) => {
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  let userGallery = userData.gallery;
+  const userRef = doc(db, "users", userData.uid);
   const [open, setOpen] = useState(false);
+  const [coverImg, setCoverImg] = useState(userData.coverImg);
+  const [selectedImg, setSelectedImg] = useState(userData.coverImg);
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
   let imgSrc = () => {
-    if (img) {
+    if (coverImg) {
+      return coverImg;
+    } else if (img) {
       return img;
     } else {
-      return "../src/assets/image-placeholder.png"; // Placeholder image
+      return "../src/assets/image-placeholder.png";
     }
   };
 
-  function openModal(event) {
+  function openModal() {
     onOpenModal();
   }
 
-  function closeModal(event) {
+  function closeModal() {
     onCloseModal();
   }
 
-  function changeCover() {
-    const userData = JSON.parse(localStorage.getItem("userData"));
+  function selectImg(imgURL) {
+    setSelectedImg(imgURL);
+  }
+
+  async function changeCover() {
+    if (selectedImg !== coverImg) {
+      setCoverImg(selectedImg);
+      let updatedUserData = userData;
+      updatedUserData.coverImg = selectedImg;
+      await updateDoc(userRef, updatedUserData);
+      localStorage.setItem("userData", JSON.stringify(updatedUserData));
+    }
+    closeModal();
   }
 
   return (
@@ -41,6 +61,7 @@ const Hero = ({ img, nextEvent, showEdit }) => {
       ) : (
         ""
       )}
+
       {showEdit ? (
         <button className="hero__edit-button" onClick={openModal}>
           Change cover image
@@ -48,8 +69,32 @@ const Hero = ({ img, nextEvent, showEdit }) => {
       ) : (
         ""
       )}
+
       <Modal open={open} onClose={closeModal} center>
-        <p>Tiny version of user gallery goes here</p>
+        <p className="change-cover__instructions">
+          Select a cover image from your gallery
+        </p>
+        <div className="change-cover__gallery-container">
+          {userGallery.length > 0
+            ? userGallery.map((img) => {
+                return (
+                  <img
+                    src={img.url}
+                    key={userGallery.indexOf(img)}
+                    className={`change-cover__img-option ${
+                      selectedImg === img.url
+                        ? "change-cover__img-option--selected"
+                        : ""
+                    }`}
+                    onClick={(e) => selectImg(img.url)}
+                  />
+                );
+              })
+            : ""}
+        </div>
+        <button className="change-cover__btn" onClick={changeCover}>
+          Save changes
+        </button>
       </Modal>
     </div>
   );
