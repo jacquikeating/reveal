364234c8ba01e0205a0fd3da2a98f3acade63526
jacquikeating <j.keating@live.ca>
2024-09-22 22:55:46 -0400

    Post component updated for Firebase
    - Commented out old Axios-related code in Post component. Will be deleted once refactor is complete.
    - Updated variable names to match naming scheme in Firestore.
    - Posts have been visually restored (correct avatar, username, timestamp converted to human readable format), but likes/comments/delete/edit have been temporarily disabled. Those functions will be updated in the next commit.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
-import {
-  getSinglePostEndpoint,
-  putPostEndpoint,
-} from "../../utils/api-utils.js";
+// import {
+//   getSinglePostEndpoint,
+//   putPostEndpoint,
+// } from "../../utils/api-utils.js";
 import "./Post.scss";
 
 // const [postLikes, setPostLikes] = useState(likes);
 // const currentLikes = Number(postLikes);
 
 const Post = ({ postData, loggedInUser }) => {
+  console.log(postData);
   let {
-    id,
-    avatar,
-    user_id,
-    user_name,
+    userAvatar,
+    userProfileURL,
+    userName,
     timestamp,
     content,
     likes,
@@ -28,44 +28,46 @@ const Post = ({ postData, loggedInUser }) => {
   }
   content = content.replace(/&#x27;/g, "'");
 
-  async function addLikes() {
-    console.log(`current likes: ${likes}`);
-    const newLikesCount = Number(likes) + 1;
-    console.log(`new likes: ${newLikesCount}`);
+  // async function addLikes() {
+  //   console.log(`current likes: ${likes}`);
+  //   const newLikesCount = Number(likes) + 1;
+  //   console.log(`new likes: ${newLikesCount}`);
 
-    const updatedPost = { ...postData, likes: newLikesCount };
-    console.log(updatedPost);
+  //   const updatedPost = { ...postData, likes: newLikesCount };
+  //   console.log(updatedPost);
 
-    try {
-      const newLikesCount = Number(likes) + 1;
-      const updatedPost = { ...postData, likes: newLikesCount };
+  //   try {
+  //     const newLikesCount = Number(likes) + 1;
+  //     const updatedPost = { ...postData, likes: newLikesCount };
 
-      await axios.put(putPostEndpoint(id), updatedPost);
-    } catch (error) {
-      console.error("Error updating item:", error);
-    }
-  }
+  //     await axios.put(putPostEndpoint(id), updatedPost);
+  //   } catch (error) {
+  //     console.error("Error updating item:", error);
+  //   }
+  // }
 
   return (
     <article className="post">
-      <Link to={`/profile/${user_id}`}>
+      <Link to={`/profile/${userProfileURL}`}>
         <img
           className="post__avatar"
-          alt={`${user_name}'s avatar`}
-          src={avatar}
+          alt={`${userName}'s avatar`}
+          src={userAvatar}
         />
       </Link>
       <div className="post__text">
         <div className="post__name-and-time">
-          <Link to={`/profile/${user_id}`}>
-            <p className="post__username">{user_name}</p>
+          <Link to={`/profile/${userProfileURL}`}>
+            <p className="post__username">{userName}</p>
           </Link>
           <p className="post__timestamp">{timestamp}</p>
         </div>
         <p className="post__content">{content}</p>
 
         <div className="post__reactions">
-          <button className="post__btn" onClick={addLikes}>
+          <button className="post__btn">
+            {" "}
+            {/* onClick={addLikes}*/}
             <img
               className="post__icon post__icon--likes"
               src="../../src/assets/icons/heart.svg"
@@ -80,7 +82,7 @@ const Post = ({ postData, loggedInUser }) => {
             <p className="post__comments">{comments.length}</p>
           </button>
 
-          {loggedInUser == user_id ? (
+          {/* {loggedInUser == user_id ? (
             <div className="post__manage">
               <button className="post__btn">
                 <img
@@ -97,7 +99,7 @@ const Post = ({ postData, loggedInUser }) => {
             </div>
           ) : (
             ""
-          )}
+          )} */}
         </div>
       </div>
     </article>
